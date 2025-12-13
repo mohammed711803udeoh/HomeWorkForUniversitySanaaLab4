82598bb0e2c7cab8637dcdac83b9a0aab597f428
Mohammed Jubran Al-Magzi <mohammed.g.almagzi@gmail.com>
2025-12-13 20:33:34 +0300

    fix:

--- main.dart
+++ main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // إزالة علامة "DEBUG"
       debugShowCheckedModeBanner: false,
       title: 'Flutter Dashboard UI',
       theme: ThemeData(primarySwatch: Colors.blue),
